36486ab6386d56e200ba1ca0b5b8c0b21628efd3
jasurkhan <97673019+jasurkhan@users.noreply.github.com>
2022-03-12 15:32:46 +0500

    add
    add_bot

---
+++ oliy_matem_bot_telegram/data/config.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = '5140257248:AAF7OigBjmyiQb4K5Ed5lzmDgevt9ded-Ag'
+ADMINS = '1518114475'
+IP = "ip"
